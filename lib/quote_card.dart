57aa24c402163047bb874904684971f736333641
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-07-20 17:28:19 +0100

    adding delete icon

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,7 +3,8 @@ import 'quotes.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({required this.quote});
+  final Function() delete;
+  QuoteCard({required this.quote, required this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -29,6 +30,13 @@ class QuoteCard extends StatelessWidget {
                   color: Colors.grey[800],
                 ),
               ),
+              SizedBox(height: 8.0),
+              TextButton.icon(
+                onPressed: delete,
+                icon: Icon(Icons.delete),
+                label: Text('delete quote'),
+              )
+
             ],
           ),
         ));
