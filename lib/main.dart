afd37902e7c9176b84c837406fd5522a02226686
Ranim-Rabah <79060004+Farah-Torkhani@users.noreply.github.com>
2023-07-20 15:16:38 +0100

    making my class with cards

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,29 @@ class _QuoteListState extends State<QuoteList> {
    
   ];
  
-  
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(10.0, 16.0, 16.0, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(quote.text,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600],
+                )),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(quote.author,
+                style: TextStyle(fontSize: 14.0, color: Colors.grey[800])),
+          ],
+        ),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,7 +59,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
         body: Column(
           children: quotes
-              .map((quote) => Text('${quote.text} - ${quote.author}'))
+              .map((quote) => quoteTemplate(quote))
               .toList(),
         )
     );
